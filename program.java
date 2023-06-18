85a80f7fbfa39bb9f5577711c8ab9951ec778ac8
SidorovK <2887458@gmail.com>
2023-06-18 21:00:48 +0300

    1st lesson of java

---
+++ program.java
@@ -0,0 +1,20 @@
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        // String s = null;
+        // System.out.println(s);
+        // float f = 123.450f;
+        // char ch = '{';
+        // System.out.println(ch);
+        
+        // boolean f true ^ false;
+        // var i = 123;    //неявная типизация
+
+        String s = "qwer";
+        
+
+    }
+    
+}
